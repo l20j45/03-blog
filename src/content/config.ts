bd9434dcae6ea6eca04f6e485c13e20366b79656
Daniel Rojas <l20j45_@gmail.com>
2024-11-18 17:41:55 -0600

    Se termino la primer parte del blog del contenido.  aqui se busco la optimizacion de imagenes, manejo de contenido.

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: ({ image }) => z.object({
+        title: z.string(),
+        date: z.date(),
+        description: z.string(),
+        image: image().refine((img) => img.width < 1200, {
+            message: "Image should be lower than 12100px",
+        }),
+        //relacion
+        author: z.string(),
+        //relacion
+        tags: z.array(z.string()),
+    })
+})
+
+export const collections = {
+    blog: blogCollection
+}
